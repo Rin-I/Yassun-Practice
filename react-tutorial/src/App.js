62dca49ee369324b523c27aebbc538fa0ed8adca
InamuraRin <75918034+InamuraRin@users.noreply.github.com>
2021-05-15 10:07:27 +0900

    出だしだけ制作

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { List } from "./components/List"
 
 function App() {
+  cosnt[(tab, setTab)] = useState("List")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav>リスト</nav>
+        <nav>フォーム</nav>
       </header>
+      <hr></hr>
+
+      <List />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
